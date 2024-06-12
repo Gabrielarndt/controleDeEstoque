e362db3655e3dbf6dfb9da4d7405051ad01cb788
Gabriel Arndt <gabrielharndt@hotmail.com>
2024-06-12 11:35:32 -0300

    up

--- web/app.js
+++ web/app.js
@@ -1,20 +1,52 @@
-angular.module('app', ['ngRoute'])
-    .constant('API_URL', 'http://localhost:8080/api/products')
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/products', {
-                templateUrl: 'app/templates/product-list.html',
-                controller: 'ProductListController'
-            })
-            .when('/products/create', {
-                templateUrl: 'app/templates/product-create.html',
-                controller: 'ProductCreateController'
-            })
-            .when('/products/edit/:id', {
-                templateUrl: 'app/templates/product-edit.html',
-                controller: 'ProductEditController'
-            })
-            .otherwise({
-                redirectTo: '/products'
-            });
-    }]);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/products', {
+        templateUrl: 'app/templates/product-list.html',
+        controller: 'ProductListController'
+    })
+    .when('/products/create', {
+        templateUrl: 'app/templates/product-create.html',
+        controller: 'ProductCreateController'
+    })
+    .when('/products/edit/:id', {
+        templateUrl: 'app/templates/product-edit.html',
+        controller: 'ProductEditController'
+    })
+    .otherwise({
+        redirectTo: '/products'
+    });
+});
+
+app.controller('ProductController', function($scope, $http) {
+    $scope.products = [];
+    $scope.product = {};
+
+    $scope.getProducts = function() {
+        $http.get('http://localhost:8080/api/products').then(function(response) {
+            $scope.products = response.data;
+        });
+    };
+
+    $scope.createProduct = function() {
+        $http.post('http://localhost:8080/api/products', $scope.product).then(function(response) {
+            $scope.product = {};
+            $scope.getProducts();
+        });
+    };
+
+    $scope.updateProduct = function() {
+        $http.put('http://localhost:8080/api/products/' + $scope.product.id, $scope.product).then(function(response) {
+            $scope.getProducts();
+        });
+    };
+
+    $scope.deleteProduct = function(id) {
+        $http.delete('http://localhost:8080/api/products/' + id).then(function(response) {
+            $scope.getProducts();
+        });
+    };
+
+    $scope.getProducts();
+});
